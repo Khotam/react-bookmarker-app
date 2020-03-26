8181d9c15bbe466b435b1e3ecf633cea9a6831af
Khotam <bakhromov17@gmail.com>
2020-03-26 16:39:06 +0500

    Updates

---
+++ precache-manifest.7d9bb19f631894538f36ca657f2654ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f2327407949870a2acfc499da7825fb",
+    "url": "/react-bookmarker-app/index.html"
+  },
+  {
+    "revision": "57ee963c7114aa64db5b",
+    "url": "/react-bookmarker-app/static/css/main.702c307e.chunk.css"
+  },
+  {
+    "revision": "3c73ae9b9f8ca0941b8b",
+    "url": "/react-bookmarker-app/static/js/2.0a9d4808.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-bookmarker-app/static/js/2.0a9d4808.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57ee963c7114aa64db5b",
+    "url": "/react-bookmarker-app/static/js/main.e29e68cc.chunk.js"
+  },
+  {
+    "revision": "c0bfb0ef6f201f573598",
+    "url": "/react-bookmarker-app/static/js/runtime-main.c7eb5f70.js"
+  }
+]);
